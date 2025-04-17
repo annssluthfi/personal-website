3e5511c2a4fafec7557ee16f46f267a3895bd03a
annssluthfi <annisalthf@gmail.com>
2025-04-17 17:02:13 +0700

    fix issue in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Home from "@/app/ui/dashboard/home"
 import Experience from "./ui/dashboard/experience";
 import Projects from "./ui/dashboard/projects";
